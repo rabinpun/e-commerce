a8a602d69e506365cb36027b2d94b56ce5044b1c
rabinpun <rabinpun49@gmail.com>
2020-09-10 11:51:38 +0545

    bootstrap debug

--- resources/views/main/layouts/master.blade.php
+++ resources/views/main/layouts/master.blade.php
@@ -24,10 +24,7 @@
 <!-- CSS only -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-<!-- JS, Popper.js, and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
     <!-- Site CSS -->
     <link rel="stylesheet" href="{{asset('frontend_assets/css/style.css')}}">
     <!-- Responsive CSS -->
@@ -57,9 +54,16 @@
 
 <a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
     <!-- ALL JS FILES -->
-<script src="{{asset('frontend_assets/js/jquery-3.2.1.min.js')}}"></script>
+{{-- <script src="{{asset('frontend_assets/js/jquery-3.2.1.min.js')}}"></script>
     <script src="{{asset('frontend_assets/js/popper.min.js')}}"></script>
-    <script src="{{asset('frontend_assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('frontend_assets/js/bootstrap.min.js')}}"></script> --}}
+
+    <!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+
     <!-- ALL PLUGINS -->
     <script src="{{asset('frontend_assets/js/jquery.superslides.min.js')}}"></script>
     <script src="{{asset('frontend_assets/js/bootstrap-select.js')}}"></script>
